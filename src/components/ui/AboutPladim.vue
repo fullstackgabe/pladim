bdede257717dcfeaaa3bf254b4126fe667fc425a
Gabe <fullstackgabe@gmail.com>
2026-09-19 21:01:37 -0300

    Frase do M: mentalize seu objetivo

--- src/components/ui/AboutPladim.vue
+++ src/components/ui/AboutPladim.vue
@@ -58,7 +58,7 @@ const STEPS = [
   'Assinale como prioridade o que é mais importante naquele dia.',
   'Distribua as tarefas na semana e não deixe nada de fora.',
   'Imprevistos acontecem, reserve um dia para eles.',
-  'Mantenha sua missão sempre à vista, para ter em mente o que precisa fazer.',
+  'Mentalize seu objetivo sendo alcançado sempre que olhar para sua missão.',
 ]
 </script>
 
