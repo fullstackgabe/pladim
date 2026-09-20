d4a7647c26c1866b7588001298fe04e1b24a97f8
Gabe <fullstackgabe@gmail.com>
2026-09-19 21:00:36 -0300

    Frase do I: reserve um dia para eles

--- src/components/ui/AboutPladim.vue
+++ src/components/ui/AboutPladim.vue
@@ -57,7 +57,7 @@ const STEPS = [
   'Leia a sua missão todos os dias até alcançar seu objetivo.',
   'Assinale como prioridade o que é mais importante naquele dia.',
   'Distribua as tarefas na semana e não deixe nada de fora.',
-  'Imprevistos acontecem, reserve um dia para resolvê-los.',
+  'Imprevistos acontecem, reserve um dia para eles.',
   'Mantenha sua missão sempre à vista, para ter em mente o que precisa fazer.',
 ]
 </script>
